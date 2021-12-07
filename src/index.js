95efceafda1eaa7bed182c7f5db3449812a7e6db
manjoeso <jmanso93@gmail.com>
2021-12-07 18:56:52 -0500

    Working with API hooked up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 // import Example from "./scripts/example";
+import {generateGraph, formatDates, createD3Data, drawGraph} from "./scripts/graph.js";
+require('./scripts/graph.js')
 document.addEventListener("DOMContentLoaded", () => {
-    require('./scripts/graph.js')
-  
+
+    generateGraph("BTC")
+
+    const liElements = document.querySelectorAll('li')
+    liElements.forEach(function(li) {
+        li.addEventListener("click", e=>{
+            d3.select("svg").remove();
+            generateGraph(li.getAttribute('id'))
+        })
+    })
+
+
 })
+
--- src/scripts/graph.js
+++ src/scripts/graph.js
@@ -1,44 +1,35 @@
-const app = document.getElementById('root')
 const axios = require('axios');
 
-// make pojo with keys as method, values 
-// could also pack into a class
-// export at the end
-// class Graph {
-//     constructor(coin){
-//         this.coin = coin;
-//     }
-// create pojo with keys as functions, ajaz twitter and TTT
 function generateGraph(coin){
     // d3.selectAll("svg > *").remove();    
     let date = new Date(); // returns date object with current date and time 
     date = date.toISOString();
     let period_id = "1DAY"
-    let time_start = "2020-10-01T00:00:00" 
+    let time_start = "2021-11-15T00:00:00" 
     let time_end = date;  // current date
-    // let limit = "1000"
+    let limit = "1000";
     let currentCoin = coin
-        // axios({
-        //     method: 'get',
-        //     url: `https://rest.coinapi.io/v1/exchangerate/${currentCoin}/USD/history?period_id=${period_id}&time_start=${time_start}&time_end=${time_end}`,//`,&limit=${limit}`,
-        //     headers: {
-        //         'X-CoinAPI-Key': '9BE11048-8939-47D8-8CEC-22E693B66137'
-        //         // 'X-CoinAPI-Key': 'B1C87260-00AD-44BC-8EFD-02DF3C6984A5'
-        //     },
-        //     data: {
-        //     }
-        // }).then(apiObject => {
-        let coinPrices = [25000, 30000, 38000, 40000, 45000, 23000, 32000];
-        let coinDates = ['2015-01-01','2015-04-01', '2016-09-01','2016-10-02', '2016-11-03', '2016-12-04', '2017-09-01'];
+        axios({
+            method: 'get',
+            url: `https://rest.coinapi.io/v1/exchangerate/${currentCoin}/USD/history?period_id=${period_id}&time_start=${time_start}&time_end=${time_end}`,//limit=${limit}`,
+            headers: {
+                'X-CoinAPI-Key': '9BE11048-8939-47D8-8CEC-22E693B66137'
+                // 'X-CoinAPI-Key': 'B1C87260-00AD-44BC-8EFD-02DF3C6984A5'
+            },
+            data: {
+            }
+        }).then(apiObject => {
+        let coinPrices = [];//[25000, 30000, 38000, 40000, 45000, 23000, 32000];
+        let coinDates = [];// ['2015-01-01','2015-04-01', '2016-09-01','2016-10-02', '2016-11-03', '2016-12-04', '2017-09-01'];
         // let coinDates = [0, 1, 2, 3, 4, 5, 6]
-            // apiObject.data.forEach(el=>{
-            //     coinPrices.push(el.rate_high)
-            //     coinDates.push(el.time_open.slice(0,10))
-            // })
+            apiObject.data.forEach(el=>{
+                coinPrices.push(el.rate_high)
+                coinDates.push(el.time_open.slice(0,10))
+            })
         let d3Data = createD3Data(formatDates(coinDates), coinPrices);
         const maxPrice = Math.max(...coinPrices);
-        drawGraph(d3Data, maxPrice, coin);
-//   });
+        drawGraph(d3Data, maxPrice, coin)
+  });
 }
 
 function formatDates(coinDates){
@@ -63,40 +54,51 @@ function drawGraph(data, maxPrice, coin){ // need to somehow "delete" the graph 
     
     const width = 1500;
     const height = 1000;
+    const yMax = maxPrice + maxPrice/10;
     
     var svg = d3.select('#d3-display')
         .append('svg')
         .attr("preserveAspectRatio", "xMinYMin meet")
         .attr("viewBox", `0 0 ${width} ${height}`)
         .classed("svg-content", true)
-        // .attr('width', width)
-        // .attr('height', height)
         .attr('id',"svg");
-    const yMax = maxPrice + maxPrice/10;
-    var xScale = d3
+    // Set the ranges
+    var x = d3
         .scaleTime()
         .domain([data[0][0], data[data.length-1][0]])
         .range([0, width/1.25])
         .nice();
-    var yScale = d3
+    var y = d3
         .scaleLinear()
         .domain([0, yMax])
         .range([height/1.5, 0])
         .nice();    
+
+    // creates x-axis
+    svg.append("g")
+        .attr("transform", "translate(150," + 775 + ")")
+        .call(d3.axisBottom(x).ticks(d3.timeDay)) //change to timeYear if need
+        .style("color", "E1D9D1"); 
+    // creates y-axis
+    svg.append("g")
+        .attr("transform", "translate(150," + 108.5 + ")")
+        .call(d3.axisLeft(y))
+        .style("color", "E1D9D1"); 
+        
+    var line = d3.line()
+        .x(function(d) { return x(d[0]); }) 
+        .y(function(d) { return y(d[1]); });
 
     svg.append('g')
         .selectAll("dot")
         .data(data)
         .enter()
         .append("circle")
-        .attr("cx", function (d) { return xScale(d[0]); } ) // x-coordinate of data point
-        .attr("cy", function (d) { return yScale(d[1]); } ) // y-coordinate of data point
+        .attr("cx", function (d) { return x(d[0]); } ) // x-coordinate of data point
+        .attr("cy", function (d) { return y(d[1]); } ) // y-coordinate of data point
         .attr("r", 2)
         .attr("transform", "translate(" + 150 + "," + 108.5 + ")")
         .style("fill", "#39FF14");
-    var line = d3.line()
-        .x(function(d) { return xScale(d[0]); }) 
-        .y(function(d) { return yScale(d[1]); }) 
         // .curve(d3.curveMonotoneX)
     svg.append("path")
         .datum(data) 
@@ -106,18 +108,13 @@ function drawGraph(data, maxPrice, coin){ // need to somehow "delete" the graph 
         .style("fill", "none")
         .style("stroke", "#39FF14")
         .style("font-weight", "bold")
-        .style("stroke-1", "2");
+        .style("stroke-1", "2")
+        // .on('mouseover', function (d, i) {
+        //     d3.select(this).transition()
+        //         .duration('50')
+        //         .attr('opacity', '.6')
+        // });
     // ---------------------------------------------------------------------
-    // creates x-axis
-    svg.append("g")
-        .attr("transform", "translate(150," + 775 + ")")
-        .call(d3.axisBottom(xScale).ticks(d3.timeYear)) //change to timeYear if need
-        .style("color", "E1D9D1"); 
-    // creates y-axis
-    svg.append("g")
-        .attr("transform", "translate(150," + 108.5 + ")")
-        .call(d3.axisLeft(yScale))
-        .style("color", "E1D9D1"); 
     // Create Title
     svg.append('text')
         .attr('x', width/2)
@@ -149,23 +146,11 @@ function drawGraph(data, maxPrice, coin){ // need to somehow "delete" the graph 
         .style('fill', '#E1D9D1')
         .text('Price ($USD)')
         .attr('font-weight', 700);
-    // Create grids.
-    
 }
-// }
+    
 // > formatDate = d3.time.format("%b-%Y")
 // > formatDate(parseDate('2003-01-01'))
 // "Jan-2003"
 // do this part on display?
 
-generateGraph("BTC")
-// grabs all list elements
-const liElements = document.querySelectorAll('li')
-// adds event listener to each li element, passing through it's id as the coin name 
-liElements.forEach(function(li) {
-    
-    li.addEventListener("click", e=>{
-        d3.select("svg").remove();
-        generateGraph(li.getAttribute('id'))
-    })
-})
+export {generateGraph, formatDates, createD3Data, drawGraph}
