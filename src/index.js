961c2673a667d2117fc63b9240e9008858a4d0f3
manjoeso <jmanso93@gmail.com>
2021-12-08 14:20:01 -0500

    Added historical data, working with graphs, presentable as is. Going to add chart functionality next

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 // import Example from "./scripts/example";
-import {generateGraph, formatDates, createD3Data, drawGraph} from "./scripts/graph.js";
+import {generateGraph, formatDates, createD3Data, drawGraph, createLabels} from "./scripts/graph.js";
+
+import {btcData} from "../coin_data/btc.js"
+import {ethData} from "../coin_data/eth.js"
+import {adaData} from "../coin_data/ada.js"
+import {solData} from "../coin_data/sol.js"
+import {dogeData} from "../coin_data/doge.js"
+// console.log(dogeData)
 require('./scripts/graph.js')
+
 document.addEventListener("DOMContentLoaded", () => {
-
-    generateGraph("BTC")
-
     const liElements = document.querySelectorAll('li')
+    generateGraph("BTC", btcData)
     liElements.forEach(function(li) {
         li.addEventListener("click", e=>{
             d3.select("svg").remove();
-            generateGraph(li.getAttribute('id'))
+            if(li.getAttribute('id') === "BTC"){
+                generateGraph(li.getAttribute('id'), btcData)
+            } else if(li.getAttribute('id') === "ETH"){
+                generateGraph(li.getAttribute('id'), ethData)
+            } else if(li.getAttribute('id') === "ADA"){
+                generateGraph(li.getAttribute('id'), adaData)
+            } else if(li.getAttribute('id') === "SOL"){
+                generateGraph(li.getAttribute('id'), solData)
+            } else if(li.getAttribute('id') === "DOGE"){
+                generateGraph(li.getAttribute('id'), dogeData)
+            }
+            
         })
     })
-
-
 })
 
--- src/scripts/graph.js
+++ src/scripts/graph.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
+// var internalData = require('../../coin_data/ada.js')
 
-function generateGraph(coin){
-    // d3.selectAll("svg > *").remove();    
+function generateGraph(coin, data){
     let date = new Date(); // returns date object with current date and time 
     date = date.toISOString();
     let period_id = "1DAY"
@@ -9,34 +9,35 @@ function generateGraph(coin){
     let time_end = date;  // current date
     let limit = "1000";
     let currentCoin = coin
-        axios({
-            method: 'get',
-            url: `https://rest.coinapi.io/v1/exchangerate/${currentCoin}/USD/history?period_id=${period_id}&time_start=${time_start}&time_end=${time_end}`,//limit=${limit}`,
-            headers: {
-                'X-CoinAPI-Key': '9BE11048-8939-47D8-8CEC-22E693B66137'
-                // 'X-CoinAPI-Key': 'B1C87260-00AD-44BC-8EFD-02DF3C6984A5'
-            },
-            data: {
-            }
-        }).then(apiObject => {
-        let coinPrices = [];// [25000, 30000, 38000, 40000, 45000, 23000, 32000];
-        let coinDates = [];//['2015-01-01','2015-04-01', '2016-09-01','2016-10-02', '2016-11-03', '2016-12-04', '2017-09-01'];
-        // let coinDates = [0, 1, 2, 3, 4, 5, 6]
-            apiObject.data.forEach(el=>{
-                coinPrices.push(el.rate_high)
-                coinDates.push(el.time_open.slice(0,10))
-            })
+    console.log(data)
+        // axios({
+        //     method: 'get',
+        //     url: `https://rest.coinapi.io/v1/exchangerate/${currentCoin}/USD/history?period_id=${period_id}&time_start=${time_start}&time_end=${time_end}`,//limit=${limit}`,
+        //     headers: {
+        //         'X-CoinAPI-Key': '9BE11048-8939-47D8-8CEC-22E693B66137'
+        //         // 'X-CoinAPI-Key': 'B1C87260-00AD-44BC-8EFD-02DF3C6984A5'
+        //     },
+        //     data: {
+        //     }
+        // }).then(apiObject => {
+        let coinPrices = []; //[25000, 30000, 38000, 40000, 45000, 23000, 32000];
+        let coinDates = []; //['2015-01-01','2015-04-01', '2016-09-01','2016-10-02', '2016-11-03', '2016-12-04', '2017-09-01'];
+        data.forEach(el=>{
+            coinPrices.push(el.rate_high)
+            coinDates.push(el.time_open.slice(0,10))
+        })
         let d3Data = createD3Data(formatDates(coinDates), coinPrices);
         const maxPrice = Math.max(...coinPrices);
+        console.log(maxPrice)
+
         drawGraph(d3Data, maxPrice, coin)
-  });
+//   });
 }
 
 function formatDates(coinDates){
     let dateNum = [];
     for(let i = 0; i < coinDates.length; i++){
         coinDates[i] = new Date(coinDates[i]) // converts dates into date objects
-        // coinDates[i] = i;
     }
     return coinDates;
 }
@@ -51,16 +52,18 @@ function createD3Data(coinDates, coinPrices){
 
 function drawGraph(data, maxPrice, coin){ // need to somehow "delete" the graph in beginning to add new graph
 
-    const width = 1500;
-    const height = 1000;
+    // const width = 1500;
+    // const height = 1000;
     const yMax = maxPrice + maxPrice/10;
-    
-    var svg = d3.select('#d3-display')
-        .append('svg')
-        .attr("preserveAspectRatio", "xMinYMin meet")
-        .attr("viewBox", `0 0 ${width} ${height}`)
-        .classed("svg-content", true)
-        .attr('id',"svg");
+
+    const margin = { top: 40, right: 80, bottom: 60, left: 50 },
+        width = 1500 - margin.left - margin.right,
+        height = 1000 - margin.top - margin.bottom;
+    // Parse the date / time
+    const parseDate = d3.timeFormat("%m/%d/%y"),
+        formatDate = d3.timeFormat("%b %d"),
+        formatMonth = d3.timeFormat("%b");
+
     // Set the ranges
     var x = d3
         .scaleTime()
@@ -72,22 +75,44 @@ function drawGraph(data, maxPrice, coin){ // need to somehow "delete" the graph 
         .domain([0, yMax])
         .range([height/1.5, 0])
         .nice();    
+    
+    const area = d3
+        .area()
+        .x((d) => { return x(d.date); })
+        .y0(height)
+        .y1((d) => { return y(d.price); })
+        .curve(d3.curveCardinal);
+
+    var valueLine = d3.line()
+        .x(function(d) { return x(d[0]); }) 
+        .y(function(d) { return y(d[1]); });
+
+    const svg = d3
+        .select('#d3-display')
+        .append('svg')
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .attr("viewBox", `0 0 ${width} ${height}`)
+        .classed("svg-content", true)
+        .attr('id',"svg")
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     // creates x-axis
     svg.append("g")
-        .attr("transform", "translate(150," + 775 + ")")
-        .call(d3.axisBottom(x).ticks(d3.timeDay)) //change to timeYear if need
+        .attr("class", "x axis")
+        .attr("transform", "translate(150," + 708.5 + ")")
+        .call(d3.axisBottom(x).tickFormat(formatMonth))//ticks(d3.timeYear))  **// NEED TO CHANGE BACK HERE POTENTIALLY
         .style("color", "E1D9D1"); 
+
     // creates y-axis
     svg.append("g")
+        .attr("class", "y axis")
         .attr("transform", "translate(150," + 108.5 + ")")
         .call(d3.axisLeft(y))
         .style("color", "E1D9D1"); 
         
-    var line = d3.line()
-        .x(function(d) { return x(d[0]); }) 
-        .y(function(d) { return y(d[1]); });
-
+    // ------------------------------------------------
+    // append data
     svg.append('g')
         .selectAll("dot")
         .data(data)
@@ -116,7 +141,7 @@ function drawGraph(data, maxPrice, coin){ // need to somehow "delete" the graph 
         .datum(data) 
         .attr("class", "line") 
         .attr("transform", "translate(" + 150 + "," + 108.5 + ")")
-        .attr("d", line)
+        .attr("d", valueLine)
         .style("fill", "none")
         .style("stroke", "#39FF14")
         .style("font-weight", "bold")
@@ -127,7 +152,13 @@ function drawGraph(data, maxPrice, coin){ // need to somehow "delete" the graph 
         //         .attr('opacity', '.6')
         // });
     // ---------------------------------------------------------------------
-    // Create Title
+    createLabels(svg, height, width, coin, margin)
+}
+
+function appendData(){}
+
+function createLabels(svg, height, width, coin, margin){
+    // title
     svg.append('text')
         .attr('x', width/2)
         .attr('y', 100)
@@ -158,16 +189,6 @@ function drawGraph(data, maxPrice, coin){ // need to somehow "delete" the graph 
         .style('fill', '#E1D9D1')
         .text('Price ($USD)')
         .attr('font-weight', 700);
-
-    // add tool tip
-    // var div = d3.select("svg").append("div")	
-    //     .attr("class", "tooltip")				
-    //     .style("opacity", 0);
 }
-    
-// > formatDate = d3.time.format("%b-%Y")
-// > formatDate(parseDate('2003-01-01'))
-// "Jan-2003"
-// do this part on display?
 
-export {generateGraph, formatDates, createD3Data, drawGraph}
+export {generateGraph, formatDates, createD3Data, drawGraph, createLabels}
